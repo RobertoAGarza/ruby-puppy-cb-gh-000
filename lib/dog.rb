4b0d6d697e707c80bf808e8b33413ba68b2021d8
Roberto Garza <robertg4567@hotmail.com>
2018-07-25 17:22:15 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog 
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initializes(name)
+    @name = name
+    @@all << self
+  end 
+
+  def all 
+    @@all.each do |dog|
+      puts dog.name
+    end 
+  end
+end 
